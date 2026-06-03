c86b4c15cddd23f1a8decbf3c2dbace45a5c44d5
muhammadhasssan2006 <hassanweb365@gmail.com>
2026-06-04 00:30:52 +0500

    C# Basic syntax

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.Common;
+
+class Program
+{
+    // -------------- Basic syntax of C# -----------
+    // => using system es used to  Tools/libraries and use console,
+    // => if we donot use console we have to write "system.Console" every time 
+    // => when i have to use console.
+    // => class program is our container of structure.
+    // => Main()-> its the main function where we will perform our task
+
+    // ==> variables
+    static void Main()
+    {
+
+        // int age = 25;
+        // int for numbers.
+        // Console.WriteLine("Age is: " + age);
+        // Console.WriteLine() for output.
+
+        // string Name = "Hassan";
+        // Console.WriteLine("Your Name is: " + Name);
+        //  string for numbers and alphabets called alphanumeric.
+
+        // double weigth = 65.7;
+        // Console.WriteLine("Your weight is: " + weigth);
+        //  double for decimal values
+
+        // bool isActive = true;
+        // Console.WriteLine(isActive);
+        //  bool for booleans (true/false)
+
+
+
+
+
+    }
+
+
+}
